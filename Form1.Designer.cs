2a551a6201b5746dc6d59cadde2e37ac38bf9f5c
AldrenKim <69776513+AldrenKim@users.noreply.github.com>
2021-02-25 00:23:45 +0800

    Second commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -41,9 +41,10 @@
             this.radioButton4 = new System.Windows.Forms.RadioButton();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -58,7 +59,7 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(245, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(17, 19);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(306, 296);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -100,18 +101,19 @@
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(55, 175);
+            this.radioButton1.Location = new System.Drawing.Point(61, 154);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(64, 17);
             this.radioButton1.TabIndex = 6;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Discrete";
             this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(14, 149);
+            this.label3.Location = new System.Drawing.Point(20, 128);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(170, 13);
             this.label3.TabIndex = 7;
@@ -120,39 +122,41 @@
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(55, 198);
+            this.radioButton2.Location = new System.Drawing.Point(61, 177);
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(50, 17);
             this.radioButton2.TabIndex = 8;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "Gaus";
             this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
             // 
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(55, 221);
+            this.radioButton3.Location = new System.Drawing.Point(61, 200);
             this.radioButton3.Name = "radioButton3";
             this.radioButton3.Size = new System.Drawing.Size(85, 17);
             this.radioButton3.TabIndex = 9;
             this.radioButton3.TabStop = true;
             this.radioButton3.Text = "Mexican Hat";
             this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
             // 
             // radioButton4
             // 
             this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(55, 244);
+            this.radioButton4.Location = new System.Drawing.Point(61, 223);
             this.radioButton4.Name = "radioButton4";
             this.radioButton4.Size = new System.Drawing.Size(78, 17);
             this.radioButton4.TabIndex = 10;
             this.radioButton4.TabStop = true;
             this.radioButton4.Text = "French Hat";
             this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.textBox3);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.radioButton4);
             this.groupBox1.Controls.Add(this.label3);
@@ -164,41 +168,48 @@
             this.groupBox1.Size = new System.Drawing.Size(210, 296);
             this.groupBox1.TabIndex = 11;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Text = "Input data";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(19, 94);
+            this.label4.Location = new System.Drawing.Point(30, 99);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(176, 13);
+            this.label4.Size = new System.Drawing.Size(144, 13);
             this.label4.TabIndex = 11;
-            this.label4.Text = "Number of neurons on Output Layer";
+            this.label4.Text = "Number of Output Layer = 64";
             // 
-            // textBox3
+            // groupBox2
             // 
-            this.textBox3.Location = new System.Drawing.Point(51, 110);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(86, 20);
-            this.textBox3.TabIndex = 12;
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Location = new System.Drawing.Point(228, 1);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(366, 541);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Visualization";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(831, 533);
+            this.ClientSize = new System.Drawing.Size(1538, 800);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -218,8 +229,8 @@
         private System.Windows.Forms.RadioButton radioButton3;
         private System.Windows.Forms.RadioButton radioButton4;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,7 +15,13 @@ namespace SOFM_KMAP
     public partial class Form1 : Form
     {
         NeuralNetwork nn;
+        int function;
+        double epsilon;
+        int iteration;
+        int output;
         Color[,] colorM;
+        System.Windows.Forms.GroupBox groupBox3;
+
         public Form1()
         {
             InitializeComponent();
@@ -23,31 +29,117 @@ namespace SOFM_KMAP
 
         private void button1_Click(object sender, EventArgs e)
         {
-            nn = new NeuralNetwork(16, 10000, 0.00001, Functions.Discrete);
-            OpenFileDialog ofd = new OpenFileDialog();
-            if (ofd.ShowDialog() == DialogResult.OK)
+            try
             {
-                nn.ReadDataFromFile(ofd.FileName);
-                Bitmap img = new Bitmap(16, 16);
-                colorM = nn.ColorSOFM();
-                for (int i = 0; i < colorM.GetLength(0); i++)
+                output = 64;//set to 64
+                iteration = Convert.ToInt32(textBox1.Text);
+                epsilon = Convert.ToDouble(textBox2.Text);
+
+                nn = new NeuralNetwork(output, iteration, epsilon, (Functions)function);
+                OpenFileDialog ofd = new OpenFileDialog();
+                if (ofd.ShowDialog() == DialogResult.OK)
                 {
-                    for (int j = 0; j < colorM.GetLength(1); j++)
+                    nn.ReadDataFromFile(ofd.FileName);
+                    nn.StartLearning();
+                    Bitmap img = new Bitmap(output, output);
+                    colorM = nn.ColorSOFM();
+                    for (int i = 0; i < colorM.GetLength(0); i++)
                     {
-                        string c = colorM[i,j].Name;
-                        if (c == "ButtonFace")
-                            c = "White";
-                        Color d = Color.FromName(c);
-                        Console.WriteLine(d);
-                        img.SetPixel(j, i, d);
+                        for (int j = 0; j < colorM.GetLength(1); j++)
+                        {
+                            Color d;
+                            string c = colorM[i, j].Name;
+                            if (c == "ButtonFace")
+                                d = Color.FromArgb(255, 226, 232, 232);
+                            else
+                                d = Color.FromName(c);
+                            img.SetPixel(j, i, d);
+                            
+                        }
                     }
+                    Bitmap img2 = new Bitmap(output*15,output*15);
+                    BitmapFilter.Scale(ref img, ref img2, output*15, output*15);
+                    pictureBox1.Image = img2;
+                    pictureBox1.Paint += new PaintEventHandler(AddLinesPaint);
+                    groupBox2.Size = new Size(pictureBox1.Height+400, pictureBox1.Width+50);
+                    CreateLegendGroup();
                 }
-                Bitmap img2 = new Bitmap(img);
-                BitmapFilter.Scale(ref img,ref  img2, 300, 300);
-                pictureBox1.Image = img2;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Input error!", "FATAL ERROR!",  MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                textBox1.Text = textBox2.Text  = "";
+                foreach( RadioButton a in groupBox1.Controls.OfType<RadioButton>())
+                {
+                    a.Checked = false;
+                }
             }
             
         }
+
+        private void CreateLegendGroup()
+        {
+
+            groupBox3 = new System.Windows.Forms.GroupBox();
+            groupBox3.Location = new Point(pictureBox1.Height + 100, 10);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(200, 200);
+            groupBox3.Text = "Legend";
+            groupBox2.Controls.Add(groupBox3);
+
+        }
+
+        private void AddLinesPaint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            int move = pictureBox1.Width / output;
+            int pos = move;
+            for (; pos < pictureBox1.Height; pos += move)
+            {
+                //vertical Line
+                g.DrawLine(new Pen(Brushes.White, 1.5f), new Point(pos,0), new Point(pos, pictureBox1.Height));
+            }
+            pos = move;
+            for (; pos < pictureBox1.Width; pos += move)
+            {
+                //horizontal Line
+                g.DrawLine(new Pen(Brushes.White, 1.5f), new Point(0, pos), new Point(pictureBox1.Width, pos));
+            }
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                function = (int)Functions.Discrete;
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked == true)
+            {
+                function = (int)Functions.Gaus;
+            }
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked == true)
+            {
+                function = (int)Functions.MexicanHat;
+            }
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton4.Checked == true)
+            {
+                function = (int)Functions.FrenchHat;
+            }
+        }
+
 
     }
 }
